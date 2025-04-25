0ec69f1b52198a5e7031876fb91b64c3a583aba2
Dayoung <lim.dayeong@gmail.com>
2025-04-25 13:27:04 +0900

    login page

---
+++ abc-tpj/src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAo0k8SN1dMatHBpFcl-a8jBRqWaIYxXew",
+  authDomain: "ai-tpj.firebaseapp.com",
+  projectId: "ai-tpj",
+  storageBucket: "ai-tpj.firebasestorage.app",
+  messagingSenderId: "1075074292177",
+  appId: "1:1075074292177:web:5cd46fec1c443b1436a8f0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
