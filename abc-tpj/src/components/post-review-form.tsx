0de894be7a73e5f7a9a30265895f9aaf9f393c34
Dayoung <lim.dayeong@gmail.com>
2025-04-29 11:31:00 +0900

    update storage

--- abc-tpj/src/components/post-review-form.tsx
+++ abc-tpj/src/components/post-review-form.tsx
@@ -1,70 +1,71 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import { useState } from "react";
-import styled from "styled-components";
-import { auth, db } from "../firebase";
+import { styled } from "styled-components";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
 
 const TextArea = styled.textarea`
-    border: 2px solid white;
-    padding: 20px;
-    border-radius: 20px;
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
     font-size: 16px;
-    color: white;
-    background-color: black;
-    width: 100%;
-    resize: none;
-    &::placeholder {
-        font-size: 16px;
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-    }
-    &:focus {
-        outline: none;
-        border-color: #ed1b24;
-    }
+  }
+  &:focus {
+    outline: none;
+    border-color: #ed1b24;
+  }
 `;
 
 const AttachFileButton = styled.label`
-    padding: 10px 0px;
-    color: #ed1b24;
-    text-align: center;
-    border-radius: 20px;
-    border: 1px solid #ed1b24;
-    font-size: 14px;
-    font-weight: 600;
-    cursor: pointer;
+  padding: 10px 0px;
+  color: #ed1b24;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #ed1b24;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
 `;
 
 const AttachFileInput = styled.input`
-    display: none;
+  display: none;
 `;
 
 const SubmitBtn = styled.input`
-    background-color: #ed1b24;
-    color: white;
-    border: none;
-    padding: 10px 0px;
-    border-radius: 20px;
-    font-size: 16px;
-    cursor: pointer;
-    &:hover,
-    &:active {
-        opacity: 0.9;
-    }
+  background-color: #ed1b24;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.9;
+  }
 `;
 
-
 export default function PostReviewForm() {
-    const [isLoading, setLoaing] = useState(false);
+    const [isLoading, setLoading] = useState(false);
     const [review, setReview] = useState("");
     const [file, setFile] = useState<File | null>(null);
     const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setReview(e.target.value);
-    }
+    };
     const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { files } = e.target;
         if (files && files.length === 1) {
@@ -76,23 +77,55 @@ export default function PostReviewForm() {
         const user = auth.currentUser;
         if (!user || isLoading || review === "" || review.length > 180) return;
         try {
-            setLoaing(true);
-            await addDoc(collection(db, "ai-tpj"), {
+            setLoading(true);
+            const doc = await addDoc(collection(db, "reviews"), {
                 review,
                 createdAt: Date.now(),
                 username: user.displayName || "Anonymous",
-                userId: user.uid
+                userId: user.uid,
             });
+            if (file) {
+                const locationRef = ref(
+                    storage,
+                    `reviews/${user.uid}-${user.displayName}/${doc.id}`
+                );
+                const result = await uploadBytes(locationRef, file);
+                const url = await getDownloadURL(result.ref);
+                await updateDoc(doc, {
+                    photo: url,
+                });
+            }
+            setReview("");
+            setFile(null);
         } catch (e) {
             console.log(e);
         } finally {
-            setLoaing(false);
+            setLoading(false);
         }
-    }
-    return <Form>
-        <TextArea rows={5} maxLength={180} onChange={onChange} value={review} placeholder="Upload your review." />
-        <AttachFileButton htmlFor="file">{file ? "IMG added ✅" : "ADD IMG"}</AttachFileButton>
-        <AttachFileInput onChange={onFileChange} type="file" id="file" accept="image/*" />
-        <SubmitBtn type="submit" value={isLoading ? "Uploading..." : "Upload review"} />
-    </Form>;
+    };
+    return (
+        <Form onSubmit={onSubmit}>
+            <TextArea
+                required
+                rows={5}
+                maxLength={180}
+                onChange={onChange}
+                value={review}
+                placeholder="Upload your review."
+            />
+            <AttachFileButton htmlFor="file">
+                {file ? "Photo added ✅" : "Add photo"}
+            </AttachFileButton>
+            <AttachFileInput
+                onChange={onFileChange}
+                type="file"
+                id="file"
+                accept="image/*"
+            />
+            <SubmitBtn
+                type="submit"
+                value={isLoading ? "Uploading..." : "Upload review"}
+            />
+        </Form>
+    );
 }
